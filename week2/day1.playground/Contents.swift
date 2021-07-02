1f1522c3b01cdbd0a1bf7ffcae22c75739814a74
alper okutan <alper@alpers-MacBook-Pro.local>
2021-07-03 01:09:25 +0300

    two types of generic

--- week2/day1.playground/Contents.swift
+++ week2/day1.playground/Contents.swift
@@ -18,3 +18,19 @@ extension Int {
 
 print(7.isPrime())
 
+
+// MARK: İki parametreli ve FARKLI tipli bir generic örneği yapınız... (T, U) ???
+
+func findIndex(ofString valueToFind: String, in array: [String]) -> Int? {
+    for (index, value) in array.enumerated() {
+        if value == valueToFind {
+            return index
+        }
+    }
+    return nil
+}
+
+let strings = ["cat", "dog", "llama", "parakeet", "terrapin"]
+if let foundIndex = findIndex(ofString: "llama", in: strings) {
+    print("The index of llama is \(foundIndex)")
+}
